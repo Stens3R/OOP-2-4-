9c775374741793c8719396e97fa84117f92fc7b5
MedzhitovRamazan <79780216790@mail.ru>
2023-06-14 17:31:57 +0300

    Задание 1(1)

--- ПЗ1/Задание 1/Задание 1/Задание 1/Program.cs
+++ ПЗ1/Задание 1/Задание 1/Задание 1/Program.cs
@@ -1,5 +1,21 @@
-﻿namespace MeasuringDevice
+﻿using MeasuringDevice;
+
+MeasureLengthDevice A = new(Units.Metric);
+
+A.StartCollecting();
+System.Threading.Thread.Sleep(5000);
+int[] data = A.GetRawData();
+foreach (int i in data)
 {
+    Console.WriteLine(i);
+}
+Console.WriteLine("===================");
+Console.WriteLine(A.MetricValue());
+Console.WriteLine(A.ImperialValue());
+
+namespace MeasuringDevice
+{
+    using DeviceController;
     public interface IMeasuringDevice
     {
         /// <summary>/// Converts the raw data collected by the measuring device into a metric value
@@ -26,11 +42,122 @@
         int[] GetRawData();
     }
 
-}
-enum Units
-{
-    Metric,
-    Imperial
+    enum Units
+    {
+        Metric,
+        Imperial
+    }
+
+    class MeasureLengthDevice : IMeasuringDevice
+    {
+        private Units unitsToUse;
+        private int[]? dataCaptured;
+        private int mostRecentMeasure;
+        private DeviceController? controller;
+        private const DeviceType measurementType = DeviceType.LENGTH;
+
+
+        public MeasureLengthDevice(Units unit)
+        {
+            unitsToUse = unit;
+        }
+
+        public int[] GetRawData()
+        {
+            return dataCaptured;
+        }
+
+        public decimal ImperialValue()
+        {
+            if (unitsToUse == Units.Imperial)
+            {
+                return mostRecentMeasure;
+            }
+            else
+            {
+                return mostRecentMeasure * 0.03937m;
+            }
+        }
+
+        public decimal MetricValue()
+        {
+            if (unitsToUse == Units.Metric)
+            {
+                return mostRecentMeasure;
+            }
+            else
+            {
+                return mostRecentMeasure * 25.4m;
+            }
+        }
+
+        public void StartCollecting()
+        {
+            controller = DeviceController.StartDevice();
+            GetMeasurements();
+        }
+
+        public void StopCollecting()
+        {
+            if (controller != null)
+            {
+                controller.StopDevice();
+                controller = null;
+            }
+        }
+
+        private void GetMeasurements()
+        {
+            dataCaptured = new int[10];
+            System.Threading.ThreadPool.QueueUserWorkItem((dummy) =>
+            {
+                int x = 0;
+                Random timer = new Random();
+
+                while (controller != null)
+                {
+                    System.Threading.Thread.Sleep(timer.Next(1000, 5000));
+                    dataCaptured[x] = controller != null ?
+                        controller.TakeMeasurement() : dataCaptured[x];
+                    mostRecentMeasure = dataCaptured[x];
+                    x++;
+                    if (x == 10)
+                    {
+                        x = 0;
+                    }
+                }
+            });
+        }
+
+    }
 }
 
-int a = 213;
+namespace DeviceController
+{
+    class DeviceController
+    {
+        private DeviceType deviceType;
+        private Random rnd = new Random();
+        public static DeviceController StartDevice()
+        {
+            DeviceController device = new();
+            device.deviceType = DeviceType.LENGTH;
+            return device;
+        }
+
+        public void StopDevice()
+        {
+
+        }
+
+        public int TakeMeasurement()
+        {
+            return rnd.Next(1, 101);
+        }
+    }
+    enum DeviceType
+    {
+        LENGTH,
+        MASS
+    }
+}
